0e2acfa4670871c01579368aa785f5b899647b2b
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-29 02:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let store = {drivers: [], passengers: [], trips: []}
 let driverId = 0
 
 class Driver {
-  constructor (name, passenger) {
+  constructor (name, passenger, trip) {
     this.id = ++driverId;
     this.name = name;
     this.passenger = passenger;
