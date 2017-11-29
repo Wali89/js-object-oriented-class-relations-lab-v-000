70e00f5fc24e4e8dfe92949e884a9c58f86d6342
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-29 01:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+let store = {drivers: [], passenger: []}
 class Driver {
   constructor (name) {
 //check to see how id is added to object with out taking ID
     this.name = name;
   }
-
+  store.drivers.push(this)
 }
