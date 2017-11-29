6ea10fc70b51cd7094ef9615b64d324e6f0dfa4b
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-29 01:48:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let store = {drivers: [], passenger: []}
+let store = {drivers: [], passenger: [], trips: []}
 
 let driverId = 0
 
@@ -8,4 +8,12 @@ class Driver {
     this.name = name;
     store.drivers.push(this);
   }
+  trips(){
+    return store.trips.filter
+  }
+}
+
+
+class Passenger {
+  constructor ()
 }
