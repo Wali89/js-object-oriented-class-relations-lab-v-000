473130726ea9cd3f444578031a6a9cee98c8dee6
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-29 02:28:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ class Passenger {
   }
   store.passengers.push(this);
   }
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
 }
 
 let tripId = 0
